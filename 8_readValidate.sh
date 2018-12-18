9ed06024ac2d657e7005f480f13c1ea84f44e155
Marco Antonio Toscano Freire <martosfre@MacBook-Pro-de-Marco.local>
2018-12-17 23:19:52 -0500

    Como validar el ingreso de información en cuanto al tamaño del campo

---
+++ 8_readValidate.sh
@@ -0,0 +1,19 @@
+# ! /bin/bash
+# Programa para ejemplificar como capturar la información del usuario y validarla
+# Autor: Marco Toscano Freire - @martosfre
+
+option=0
+backupName=""
+clave=""
+
+echo "Programa Utilidades Postgres"
+# Acepta el ingreso de información de solo un caracter
+read -n1 -p "Ingresar una opción:" option
+echo -e "\n"
+read -n10 -p "Ingresar el nombre del archivo del backup:" backupName
+echo -e "\n"
+echo "Opción:$option , backupName:$backupName"
+read -s -p "Clave:" clave
+echo "Clave: $clave"
+
+
