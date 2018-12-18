aed4b4bf57d291c162ef533169991e2f6decf049
Marco Antonio Toscano Freire <martosfre@MacBook-Pro-de-Marco.local>
2018-12-17 23:14:39 -0500

    Pasar argumentos a un programa y recuperarlos

---
+++ 4_argumentos.sh
@@ -0,0 +1,10 @@
+# ! /bin/bash
+# Programa para ejemplificar el paso de argumentos
+# Autor: Marco Toscano Freire - @martosfre
+
+nombreCurso=$1
+horarioCurso=$2
+
+echo "El nombre del curso es: $nombreCurso dictado en el horario de $horarioCurso"
+echo "El número de parámetros enviados es: $#"
+echo "Los parámetros enviados son: $*"
