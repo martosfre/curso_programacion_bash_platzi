3c5e87d0e91023da6ab5f0b404232ce939f708d6
Marco Antonio Toscano Freire <martosfre@MacBook-Pro-de-Marco.local>
2018-12-17 22:59:08 -0500

    Creación Primer Script, comentarios

---
+++ 1_comments.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# PROGRAMA: U-POSG
+echo "Programa Utilidades Postgres"
+    <<"COMENTARIO 1"
+    Programa para administrar las utilidades de la Base
+    de Datos Postgres
+   "COMENTARIO 1"
+    
+exit 0
---
+++ 1_utilityPostgres.sh
@@ -0,0 +1,4 @@
+# !/bin/bash
+# Programa para realizar algunas operaciones utilitarios de Postgres
+
+echo "Hola bienvenido al curso de Programación bash"
