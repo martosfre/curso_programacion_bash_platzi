570e0307d92d8f8bcce50e30abcb7c0515262c02
Marco Antonio Toscano Freire <martosfre@MacBook-Pro-de-Marco.local>
2018-12-17 23:22:12 -0500

    Como pasar parámetros y opciones a un programa

---
+++ 9_options.sh
@@ -0,0 +1,21 @@
+# ! /bin/bash
+# Programa para ejemplificar como se realiza el paso de opciones con sin parámetros
+# Autor: Marco Toscano - @martosfre
+
+
+echo "Programa Opciones"
+echo "Opción 1 enviada: $1"
+echo "Opción 2 enviada: $2"
+echo "Opción enviadas: $*"
+echo -e "\n"
+echo "Recuperar valores"
+while [ -n "$1" ]
+do
+case "$1" in
+-a) echo "-a option utilizada";;
+-b) echo "-b option utilizada";;
+-c) echo "-c option utlizada";;
+*) echo "$! no es una opción";;
+esac
+shift
+done
