11cd625b5362349a8cc3c77386fc60995cd661b5
Marco Antonio Toscano Freire <martosfre@MacBook-Pro-de-Marco.local>
2018-12-17 23:24:22 -0500

    Descargar información de internet desde un programa bash

---
+++ 10_download.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+# Programa para ejemplificar el uso de la descarga de información desde internet utilizando el comando wget
+# Autor: Marco Toscano Freire - @martosfre
+
+echo "Descargar información de internet"
+wget https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.35/bin/apache-tomcat-8.5.35.zip
